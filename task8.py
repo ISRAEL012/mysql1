b7e22b5cf007b5ef9977b56b6199ec65b89c241a
ISRAEL012 <59837749+ISRAEL012@users.noreply.github.com>
2020-06-15 18:48:48 -0400

    Create task8.py

---
+++ task8.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'labo',
+    database = 'pydb_demo'
+)
+
+mycursor = db.cursor()
+
+mycursor.execute('DROP BATABASE IF EXISTS pydb_demo')
